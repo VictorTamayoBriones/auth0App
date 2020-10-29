f441f484584e38ab28e6c7a128b49d016608abaf
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-28 18:35:03 -0600

    moduleAuth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { AuthModule } from '@auth0/auth0-angular'
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-2c8gvwcr.us.auth0.com',
+      clientId: 's8xahucxPbbsdVUILflaytrFTcSPkpgf'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
